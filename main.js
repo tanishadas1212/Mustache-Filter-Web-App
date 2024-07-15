a85c04bead72c367a0175cc3291137f18afaaf07
AveryHills_0 <110232710+tanishadas1212@users.noreply.github.com>
2024-07-15 16:43:33 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,4 +1,8 @@
+noseX = 0;
+noseY =0;
+
 function preload() {
+    clown_nose = loadImage('https://i.postimg.cc/3x3QzSGq/m.png')
 }
 
 function setup() {
@@ -21,13 +25,16 @@ function gotPoses(results)
     if(results.length > 0)
     {
         console.log(results);
-        console.log("nose x = " + results[0].pose.nose.x);
-        console.log("nose y = " + results[0].pose.nose.y);
+        noseX = results[0].pose.nose.x-15;
+        noseY = results[0].pose.nose.y+5;
+        console.log("nose x = " + noseX);
+        console.log("nose y = " + noseY);
     }
 }
 
 function draw() {
     image(video, 0, 0, 300, 300);
+    image(clown_nose, noseX, noseY, 30, 30);
 }
 
 function take_snapshot() {
